f35b6af4ce596f5e954ba83c5bdf741b58795d4a
Oliver Allen <153258506+saber-dowsing@users.noreply.github.com>
2024-01-10 18:29:11 +0700

    Create Transaction.h

---
+++ Transaction.h
@@ -0,0 +1,11 @@
+// Transaction.h
+
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+class Transaction {
+public:
+    static void performTransaction(Account& account, double amount);
+};
+
+#endif // TRANSACTION_H
